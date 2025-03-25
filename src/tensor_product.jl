4b1881a25ff8add4dbf8bb107bc53f18a5b7d899
Olivier Gauthé <ogauthe@flatironinstitute.org>
2025-03-25 19:21:01 -0400

    fix is_offset_axis

--- src/tensor_product.jl
+++ src/tensor_product.jl
@@ -2,10 +2,10 @@
 # https://en.wikipedia.org/wiki/Tensor_product
 
 # ==================================  misc  ================================================
-is_offset_axis(a::AbstractUnitRange) = isone(first(a))
+is_offset_axis(a::AbstractUnitRange) = !isone(first(a))
 
 function require_one_based_axis(a::AbstractUnitRange)
-  return !is_offset_axis(a) && throw(ArgumentError("Range must be one-based"))
+  return is_offset_axis(a) && throw(ArgumentError("Range must be one-based"))
 end
 
 # ==============================  tensor product  ==========================================
