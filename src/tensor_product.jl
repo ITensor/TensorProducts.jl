dec5a3701b4193188c8b4f9d27995dea31f1cad0
Olivier Gauthé <ogauthe@flatironinstitute.org>
2025-03-25 16:29:38 -0400

    helper functions

--- src/tensor_product.jl
+++ src/tensor_product.jl
@@ -1,6 +1,14 @@
 # This files defines an interface for the tensor product of two axes
 # https://en.wikipedia.org/wiki/Tensor_product
 
+# ==================================  misc  ================================================
+is_offset_axis(a::AbstractUnitRange) = isone(first(a))
+
+function require_one_based_axis(a::AbstractUnitRange)
+  return !is_offset_axis(a) && throw(ArgumentError("Range must be one-based"))
+end
+
+# ==============================  tensor product  ==========================================
 ⊗() = tensor_product()
 ⊗(a) = tensor_product(a)
 
@@ -16,8 +24,7 @@ tensor_product(a1, a2, as...) = tensor_product(tensor_product(a1, a2), as...)
 
 # default
 function tensor_product(a1::AbstractUnitRange, a2::AbstractUnitRange)
-  !(isone(first(a1)) && isone(first(a2))) &&
-    throw(ArgumentError("Ranges must be one-based"))
+  require_one_based_axis(a1) || require_one_based_axis(a2)
   return Base.OneTo(length(a1) * length(a2))
 end
 
