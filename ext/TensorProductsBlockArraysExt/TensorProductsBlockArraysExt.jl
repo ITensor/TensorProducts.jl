50d1a1993c5c0185ab79836569dadefcb0b3f36c
Olivier Gauthé <ogauthe@flatironinstitute.org>
2025-03-26 09:23:14 -0400

    remove fuse_blocklengths

--- ext/TensorProductsBlockArraysExt/TensorProductsBlockArraysExt.jl
+++ ext/TensorProductsBlockArraysExt/TensorProductsBlockArraysExt.jl
@@ -14,18 +14,12 @@ using TensorProducts: OneToOne, TensorProducts
 # BlockArrays default crashes for OneToOne{Bool}
 BlockArrays.blockaxes(a::OneToOne) = (Block.(a),)
 
-function fuse_blocklengths(x::Integer, y::Integer)
-  # return blocked unit range to keep non-abelian interface
-  return blockedrange([x * y])
-end
-
 function TensorProducts.tensor_product(
   a1::AbstractBlockedUnitRange, a2::AbstractBlockedUnitRange
 )
-  nested = map(Iterators.flatten((Iterators.product(blocks(a1), blocks(a2)),))) do it
-    return mapreduce(length, fuse_blocklengths, it)
+  new_blocklengths = mapreduce(vcat, Iterators.product(blocks(a1), blocks(a2))) do (x, y)
+    return length(x) * length(y)
   end
-  new_blocklengths = mapreduce(blocklengths, vcat, nested)
   return blockedrange(new_blocklengths)
 end
 
