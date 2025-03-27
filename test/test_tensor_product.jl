8eebacaef95d270ff3aa3804fbaecbf974e0a25c
Olivier Gauthé <ogauthe@flatironinstitute.org>
2025-03-27 10:12:06 -0400

    `⊗` as tensor_product alias (#8)

--- test/test_tensor_product.jl
+++ test/test_tensor_product.jl
@@ -7,18 +7,6 @@ using BlockArrays: blockedrange, blockisequal
 r0 = OneToOne()
 b1 = blockedrange([1, 2])
 
-@testset "⊗" begin
-  @test ⊗() isa OneToOne
-  @test ⊗(1:2) == 1:2
-  @test ⊗(1:2, 1:3) == 1:6
-  @test ⊗(1:2, 1:3, 1:4) == 1:24
-
-  @test ⊗(r0, r0) isa OneToOne
-  @test blockisequal(⊗(b1, b1), blockedrange([1, 2, 2, 4]))
-  @test blockisequal(⊗(b1, r0), b1)
-  @test blockisequal(⊗(r0, b1), b1)
-end
-
 @testset "tensor_product" begin
   @test tensor_product() isa OneToOne
   @test tensor_product(1:2) == 1:2
@@ -30,5 +18,11 @@ end
   @test_throws ArgumentError tensor_product(2:3, 2:2)
 
   @test tensor_product(r0, r0) isa OneToOne
+  @test blockisequal(tensor_product(b1, r0), b1)
+  @test blockisequal(tensor_product(r0, b1), b1)
+
   @test blockisequal(tensor_product(b1, b1), blockedrange([1, 2, 2, 4]))
+  @test blockisequal(tensor_product(b1, b1), blockedrange([1, 2, 2, 4]))
+
+  @test ⊗(r0, r0) isa OneToOne
 end
