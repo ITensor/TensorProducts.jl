862668baad4f79a8b942a5376354b1cdddbb55a8
Matt Fishman <mtfishman@users.noreply.github.com>
2025-03-27 14:46:30 -0400

    Update template (#9)

--- docs/make.jl
+++ docs/make.jl
@@ -10,9 +10,9 @@ makedocs(;
   authors="ITensor developers <support@itensor.org> and contributors",
   sitename="TensorProducts.jl",
   format=Documenter.HTML(;
-    canonical="https://ITensor.github.io/TensorProducts.jl",
+    canonical="https://itensor.github.io/TensorProducts.jl",
     edit_link="main",
-    assets=["assets/favicon.ico"],
+    assets=["assets/favicon.ico", "assets/extras.css"],
   ),
   pages=["Home" => "index.md", "Reference" => "reference.md"],
 )
